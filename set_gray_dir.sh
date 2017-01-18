f46cbf439e4d9d3fd1356767a5c19220837b613a
David Freese <dfreese@stanford.edu>
2017-01-17 18:27:35 -0800

    BUG: fixup sourcing script

--- set_gray_dir.sh
+++ set_gray_dir.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 
-if [ -e Gray ]; then
-    if [ ! -h Gray ]; then
-        echo "Error: Gray in this folder is not a symbolic link.  Check this."
-    fi
-else
-    ln -s workspace/Gray/Gray
+# Get the working directoy of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -x "$DIR/workspace/Gray/Debug/Gray" ]; then
+    # Xcode Structure
+    gray_exe="$DIR/workspace/Gray/Debug/Gray"
+elif [ -x "$DIR/workspace/Gray/Gray" ]; then
+    # Make structure
+    gray_exe="$DIR/workspace/Gray/Gray"
 fi
 
-export GRAY_INCLUDE=${PWD}/GRAY_GEN_MATERIAL
+sym_link_loc="$DIR/Gray"
+
+# Check if the symbolic link exists already
+if [ -L "$sym_link_loc" ]; then
+    # Then check to see if it points to the right place, and delete it if it
+    # doesn't
+    if [ ! "$sym_link_loc" -ef "$gray_exe" ]; then
+        echo "deleting it"
+        rm "$sym_link_loc"
+    fi
+elif [ -e "$sym_link_loc" ]; then
+    # If we didn't find a symbolic link and a file exists, warn the user
+    echo "Error: Gray in this folder is not a symbolic link.  Check this."
+fi
+if [ ! -e "$sym_link_loc" ]; then
+    ln -s $gray_exe $sym_link_loc
+fi
+
+export GRAY_INCLUDE=${DIR}/GRAY_GEN_MATERIAL
