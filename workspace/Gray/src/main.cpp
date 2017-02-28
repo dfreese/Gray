cf6234c3425eee90a6856e5727ceaba5e5ac3491
David Freese <dfreese@stanford.edu>
2017-02-28 12:21:06 -0800

    MAINT: clean up main functions

--- workspace/Gray/src/main.cpp
+++ workspace/Gray/src/main.cpp
@@ -1,59 +1,16 @@
-#include <stdlib.h>
-#include <string.h>
-
-#ifdef _WIN32
-#include <windows.h>
-#endif
-
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
-
-#include <Gray/Config.h>
-#include <PixelArray/PixelArray.h>
-#include <Graphics/ViewableBase.h>
-#include <Graphics/DirectLight.h>
-#include <Graphics/CameraView.h>
-#include <KdTree/KdTree.h>
+#include <iostream>
 #include <Graphics/SceneDescription.h>
-#include <Gray/GammaRayTrace.h>
-#include <Gray/LoadMaterials.h>
-#include <Random/Random.h>
-#include <Gray/LoadDetector.h>
 #include <GraphicsTrees/IntersectionKdTree.h>
 #include <GraphicsTrees/ShadowKdTree.h>
-#include <string>
-#include <sstream>
+#include <Gray/GammaRayTrace.h>
+#include <Gray/LoadMaterials.h>
+#include <Gray/LoadDetector.h>
+#include <Gray/Config.h>
 #include <Gray/Viewer.h>
+#include <Random/Random.h>
 
-bool InitializeSceneGeometry(GammaRayTrace & Gray,
-                             const Config & config,
-                             SceneDescription & FileScene)
-{
-    // Define the lights, materials, textures and viewable objects.
-    if (!LoadMaterials::LoadPhysicsFiles(FileScene)) {
-        exit(-1);
-    }
-    LoadDetector myLoader;
-    if (!myLoader.Load(config.filename_detector, FileScene, Gray)) {
-        fprintf(stderr, "Loading file \"%s\" failed\n",
-                config.filename_detector.c_str());
-    }
-    Gray.SetFileNameOutput(config.filename_output);
-    if (config.seed != 0) {
-        Random::Seed(config.seed);
-        printf("Seeding Gray: %ld\n", config.seed);
-    }
-    return(true);
-}
+using namespace std;
 
-//**********************************************************
-// Main Routine
-// Set up OpenGL, hook up callbacks, define RayTrace world,
-// and start the main loop
-//**********************************************************
 int main( int argc, char** argv)
 {
     Config config;
@@ -64,9 +21,19 @@ int main( int argc, char** argv)
 
     GammaRayTrace Gray;
     SceneDescription FileScene;
-    if (!InitializeSceneGeometry(Gray, config, FileScene)) {
+    if (!LoadMaterials::LoadPhysicsFiles(FileScene)) {
         return(1);
     }
+    LoadDetector myLoader;
+    if (!myLoader.Load(config.filename_detector, FileScene, Gray)) {
+        cerr << "Loading file \"" << config.filename_detector << "\" failed" << endl;
+    }
+    Gray.SetFileNameOutput(config.filename_output);
+    if (config.seed != 0) {
+        Random::Seed(config.seed);
+        cout << "Seeding Gray: " << config.seed << endl;
+    }
+
     IntersectKdTree intersect_kd_tree(FileScene);
     Gray.SetKdTree(intersect_kd_tree);
 
