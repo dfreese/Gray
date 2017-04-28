9e0115d640de2928525fa42d4f7afd77068a6cbf
David Freese <dfreese@stanford.edu>
2017-04-28 08:17:46 -0700

    MAINT: add test for pet benchmark

---
+++ bench_test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+Gray -b -s 5489 -f detectors/pet_benchmark_fast.dff -i pet_benchmark_fast.dat \
+    -t 0.1
+
+diff -sq pet_benchmark_fast.dat detectors/ref_pet_benchmark_fast.dat
+
+if [ "$?" -eq 0 ]; then
+    echo ""
+    echo "       _"
+    echo "      /(|"
+    echo "     (  :"
+    echo "    __\  \  _____"
+    echo "   (____)  \`|"
+    echo "   (____)|   |"
+    echo "   (____).__|"
+    echo "   (___)__.|_____"
+    echo ""
+fi
