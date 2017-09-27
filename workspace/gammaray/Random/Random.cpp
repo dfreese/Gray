853ce1edbd5626ba5085889a9ffa78fa3a5203f7
David Freese <dfreese@stanford.edu>
2017-09-26 21:58:03 -0700

    MAINT: use Transforms in Random

--- workspace/gammaray/Random/Random.cpp
+++ workspace/gammaray/Random/Random.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <ctime>
 #include <Random/Random.h>
+#include <Random/Transform.h>
 #include <VrMath/LinearR3.h>
 
 std::mt19937 Random::generator;
@@ -55,47 +56,13 @@ void Random::RankReseed(int rank) {
 
 void Random::UniformSphere(VectorR3 & p)
 {
-    double cost = 2.0 * Random::Uniform() - 1.0;
-    double phi = 2 * M_PI * Random::Uniform();
-    double sint = std::sqrt(1.0 - cost * cost);
-    p.x = sint * std::cos(phi);
-    p.y = sint * std::sin(phi);
-    p.z = cost;
-    p.Normalize();
+    p = Transform::UniformSphere(Random::Uniform(), Random::Uniform());
 }
 
 void Random::Acolinearity(const VectorR3 & b, VectorR3 &r, double radians)
 {
-    // Theta: angle relative to other photon, mean around 180.0 degrees
-    // Phi: Angle around central axis any of 360 degrees in a circle
-    double phi = 2 * M_PI * Random::Uniform();
-    double theta = M_PI;
-
-    // Generate acolinearity angle away from blue photon
-    RotationMapR3 rotation;
-
-    // Create rotation axis this is perpendicular to Y axis
-    // using cross product and rotate red direction
-    VectorR3 rot_axis = b;
-    VectorR3 UnitY;
-    UnitY.SetUnitY();
-    rot_axis *= UnitY;
-    rot_axis.Normalize();
-
-    double g1 = Random::Gaussian();
-
-    // blur theta by acolinearity
-    theta += (g1 * radians);
-    //theta += radians;
-
-    // rotate red theta degrees away from blue
-    r = b;
-    rotation.Set(rot_axis, theta);
-    rotation.Transform(&r);
-
-    // using original blue axis rotate red around blue by 360 degrees
-    rotation.Set(b,phi);
-    rotation.Transform(&r);
+    r = Transform::Acolinearity(b, radians, Random::Uniform(),
+                                Random::Gaussian());
 }
 
 
