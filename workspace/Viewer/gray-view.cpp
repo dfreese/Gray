c7690884355244eb9edeeb1d3c40c7f151ec43e5
David Freese <dfreese@stanford.edu>
2017-12-07 07:29:31 -0800

    fix up gray view to use physics file

--- workspace/Viewer/gray-view.cpp
+++ workspace/Viewer/gray-view.cpp
@@ -24,19 +24,18 @@ int main(int argc, char ** argv)
     DetectorArray detector_array;
     SceneDescription scene;
     SourceList sources;
-    if (!sources.LoadIsotopes(config.get_isotopes_filename())) {
-        cerr << "Unable to load isotopes file: \""
-        << config.get_isotopes_filename() << "\"\n"
-        << "Check GRAY_INCLUDE env variable or specify name with --iso"
+    if (!sources.LoadIsotopes(config.get_physics_filename())) {
+        cerr << "Unable to load physics file: \""
+        << config.get_physics_filename() << "\"\n"
+        << "Check GRAY_INCLUDE env variable or specify name with --phys"
         << endl;
         return(1);
     }
-    if (!LoadMaterials::LoadPhysicsFiles(scene,
-                                         config.get_materials_filename()))
+    if (!LoadMaterials::LoadPhysicsJson(scene, config.get_physics_filename()))
     {
-        cerr << "Unable to load materials file: \""
-        << config.get_materials_filename() << "\"\n"
-        << "Check GRAY_INCLUDE env variable or specify name with --mat"
+        cerr << "Unable to load physics file: \""
+        << config.get_physics_filename() << "\"\n"
+        << "Check GRAY_INCLUDE env variable or specify name with --phys"
         << endl;
         return(1);
     }
