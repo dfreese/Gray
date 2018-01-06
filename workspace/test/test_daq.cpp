b5f37136148a75eead56c96aacfc59f47f7c4a52
David Freese <dfreese@stanford.edu>
2018-01-06 00:53:05 -0800

    TST: test basic merge cases and stats

---
+++ workspace/test/test_daq.cpp
@@ -0,0 +1,60 @@
+#include "gtest/gtest.h"
+#include <memory>
+#include "Daq/Mapping.h"
+#include "Daq/Process.h"
+#include "Daq/ProcessFactory.h"
+#include "Physics/Interaction.h"
+
+TEST(MergeTest, BasicMergeFirst) {
+    ProcessFactory::ProcessDescription desc;
+    ProcessFactory::ProcessDescLine("merge detector 1.0 first", desc);
+    // ProcessFactory::ProcessDescLine("merge detector 1.0 max", desc);
+    Mapping::IdMappingT mapping = {{"detector", {0}}};
+
+    auto proc = ProcessFactory::ProcessFactory(desc, mapping);
+
+    std::vector<Interaction> events(4);
+    std::vector<Process::TimeT> times({0.0, 0.99, 3.0, 4.0});
+    std::vector<float> energy({1.0, 2.0, 3.0, 4.0});
+
+    for (size_t ii = 0; ii < events.size(); ++ii) {
+        events[ii].time = times[ii];
+        events[ii].energy = energy[ii];
+    }
+
+    auto ready = proc->process(events.begin(), events.end());
+    EXPECT_EQ(ready, events.end() - 1);
+
+    proc->stop(ready, events.end());
+    EXPECT_EQ(proc->no_dropped(), 1);
+    EXPECT_EQ(proc->no_kept(), 3);
+    EXPECT_EQ(events[1].dropped, true);
+    EXPECT_EQ(events[0].energy, energy[0] + energy[1]);
+}
+
+TEST(MergeTest, BasicMergeMax) {
+    ProcessFactory::ProcessDescription desc;
+    ProcessFactory::ProcessDescLine("merge detector 1.0 max", desc);
+    Mapping::IdMappingT mapping = {{"detector", {0}}};
+
+    auto proc = ProcessFactory::ProcessFactory(desc, mapping);
+
+    std::vector<Interaction> events(4);
+    std::vector<Process::TimeT> times({0.0, 0.99, 3.0, 4.0});
+    std::vector<float> energy({1.0, 2.0, 3.0, 4.0});
+
+    for (size_t ii = 0; ii < events.size(); ++ii) {
+        events[ii].time = times[ii];
+        events[ii].energy = energy[ii];
+    }
+
+    auto ready = proc->process(events.begin(), events.end());
+    EXPECT_EQ(ready, events.end() - 1);
+
+    proc->stop(ready, events.end());
+    EXPECT_EQ(proc->no_dropped(), 1);
+    EXPECT_EQ(proc->no_kept(), 3);
+    EXPECT_EQ(events[0].dropped, true);
+    EXPECT_EQ(events[1].energy, energy[0] + energy[1]);
+}
+
